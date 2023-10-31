3f7c9e0d1030864bf03f8c30324d74553ed86ab9
taylersala <taylersala@gmail.com>
2023-10-31 13:38:11 -0700

    fixed quiz

--- frontend/components/Quiz.js
+++ frontend/components/Quiz.js
@@ -17,28 +17,6 @@ function Quiz(props) {
     setSelectedAnswer(answerId);
   };
 
-// leaving here for sanity reasons was messing around with the last two tests but it breaks
- // const handleQuizSubmit = async () => {
-  //   if (selectedAnswer !== null) {
-  //     const response = await postAnswer(quiz.data.quiz_id, selectedAnswer);
-  
-  //     if (response.success) {
-  //       setMessage('Your answer is correct!');
-  //     } else {
-  //       setMessage('Your answer is incorrect. Try again.');
-  //     }
-  
-  //     fetchQuiz();
-  //     setSelectedAnswer(null);
-  //   }
-  // };
-
-
-
-
-
-
-
   const handleQuizSubmit = () => {
     if (selectedAnswer !== null) {
       postAnswer(quiz.data.quiz_id, selectedAnswer)
@@ -57,15 +35,13 @@ function Quiz(props) {
 
           <div id="quizAnswers">
             {quiz.data.answers.map((answer, index) => (
-              <div key={index} className="answer">
+              <div key={index} className={`answer ${selectedAnswer === answer.answer_id ? 'selected' : ''}`}>
                 {answer.text}
                 <button
                   onClick={() => handleAnswerSelect(answer.answer_id)}
-                  className={selectedAnswer === answer.answer_id ? "selected" : ""}
                 >
                   {selectedAnswer === answer.answer_id ? 'SELECTED' : 'Select'}
                 </button>
-
               </div>
             ))}
           </div>
